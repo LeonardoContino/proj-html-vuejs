306aae71bf55e4b5217ca4d805a7dd0b349301c0
LeonardoContino <leoconti2000@hotmail.it>
2023-01-31 10:52:02 +0100

    importato fontawsome

--- src/main.js
+++ src/main.js
@@ -2,4 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import "@fontsource/montserrat";
 import 'bootstrap/dist/css/bootstrap.min.css'
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
 createApp(App).mount('#app')
