7f5c8ad6405d9d5d3b0b9cf3e6c303e45cd30b4a
LeonardoContino <leoconti2000@hotmail.it>
2023-02-02 12:02:41 +0100

    braised done

--- src/components/main/braised.vue
+++ src/components/main/braised.vue
@@ -1,5 +1,16 @@
 <script>
-
+import buttom from './buttom.vue';
+export default{
+    components:{buttom},
+    data(){
+        return{
+            title: 'BRAISED ABALONE',
+            price: '$52',
+            text: 'pretium accumsan cporttitor viverra leo gravida mollis imperdiet. frigilla nibh pharetra sociis leo amet',
+            btn: 'VIEW IN THE MENU'
+        }
+    }
+}
 </script>
 
 
@@ -7,6 +18,10 @@
     <div class="thumb">
         <img src="/images/hero42x.jpg" alt="">
         <div class="description">
+            <div class="title-price"><h3>{{ title }} </h3><span>{{ price }}</span></div>
+            <div class="s-border"></div>
+            <p>{{ text }}</p>
+            <buttom :btn="btn"></buttom>
 
         </div>
     </div>
@@ -16,10 +31,28 @@
 
 <style scoped lang="scss">
 
+.s-border{
+        border-bottom: 1px solid #B9B9B8;
+        width: 50px;
+        margin: 30px 0;
+    }
+    a{
+    padding: 5px 10px;
+    text-align: center;
+    font-size: 15px;
+    text-decoration: none;
+    color: white;
+    border: 1px solid #B9B9B8;
+    width: 200px;
+     }
+
 .thumb{
     height: 700px;
     margin: 100px 0;
     position: relative;
+    color: white;
+
+    
     img{
         height: 100%;
         width: 100%;
@@ -32,6 +65,25 @@
         width: 400px;
         bottom: calc(350px - 225px);
         left: 300px;
+        padding: 50px;
+        .title-price{
+        display: flex;
+        
+        h3{
+            flex-basis: 50%;
+        }
+        span{
+            position: absolute;
+            right: 50px;
+            
+        }
+       
+
+    }
+    p{
+            color: #B9B9B8;
+            padding:30px 0;
+        }
     }
 }
 </style>
